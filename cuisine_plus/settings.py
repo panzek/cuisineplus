3b255cb377d0cf2900337aaa136a35aaa3f2313e
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-07 11:34:40 +0100

    Add localhost IP address to allowed_hosts

--- cuisine_plus/settings.py
+++ cuisine_plus/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cuisine-plus.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cuisine-plus.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
