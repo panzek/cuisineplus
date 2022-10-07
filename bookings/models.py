8ab560bc1d8ec5bf8066c352dc49e77c1e67d6ef
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-07 15:57:44 +0100

    Update code to return string from method

--- bookings/models.py
+++ bookings/models.py
@@ -34,4 +34,7 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField()
     additional_info = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return str(self.user)
     
