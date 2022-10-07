7fd5c19cf4f083adcc2f6c32675085c615bd51f5
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-07 15:48:27 +0100

    Create booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from restaurant.models import Restaurant
+# from cuisine.models import Cuisine
 
-# Create your models here.
+class Booking(models.Model):
+    '''Booking model
+    ---
+    Attributes:
+        user: User that made the booking
+        number_of_guests: Number of guests expected
+        restaurant: Name of the restaurant being booked
+        cuisine: Menu of being booked
+        table_number: Table number
+        date: Date of the booking
+        time: Time of the booking
+        additional_info: Any additional information provided
+
+    '''
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='bookings'
+        )
+    number_of_guests = models.IntegerField()
+    # restaurant = models.ForeignKey(
+    #     Restaurant, on_delete=models.CASCADE, related_name='bookings'
+    #     )
+    restaurant = models.CharField(max_length=100)
+    # cuisine = models.ForeignKey(
+    #     Cuisine, on_delete=models.CASCADE, related_name='bookings'
+    #     )
+    cuisine = models.CharField(max_length=100)
+    table_number = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    additional_info = models.CharField(max_length=500, blank=True)
+    
