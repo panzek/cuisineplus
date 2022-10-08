76edbf4925c6deaa8b3c3e889279e0a1214b9887
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-08 18:37:58 +0100

    Update urls.py to fix TypeError issue

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookingList.as_view, name='Bookings-detail'),
+    path('', views.BookingList.as_view(), name='Bookings-detail'),
 ]
