b277f97aa6b56c6ed8693f32af30e285be68fc66
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-08 15:14:07 +0100

    Create instance of booking model

---
+++ bookings/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.BookingList.as_view, name='Bookings-detail'),
+]
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
+class BookingList(generic.ListView):
+    model = Booking
+    # queryset = Booking.objects.filter(status=1).order_by('-created_on')
+    template_name = 'booking_detail.html'
