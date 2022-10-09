96e053e9133b36520f1c2a7154cc734c6d30265d
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-09 16:33:39 +0100

    Update views.py with context to add data to template

--- bookings/views.py
+++ bookings/views.py
@@ -6,5 +6,14 @@ class BookingList(generic.ListView):
     model = Booking
     # queryset = Booking.objects.filter(status=1).order_by('-created_on')
     template_name = 'bookings/booking_detail.html'
+    # a template location: /path/to/project/bookings/templates/bookings/booking_detail.html
+
+    context = {
+        'bookings': Booking.objects.all()
+    }
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(BookingList, self).get_context_data(**kwargs)
+    #     return context
 
 
