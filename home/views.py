3ef050df49ef0218c268fa40f81ebd9d2ddb601d
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-11 12:00:42 +0100

    Update views.py to render the homepage

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    """
+    View to display the site's home page
+    """
+    template_name = 'home/index.html'
