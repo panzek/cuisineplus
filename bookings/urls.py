66f9f9e03861145b2ef3de10e4d683be817ac435
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-12 13:38:36 +0100

    Update bookings urls to fix 404 error

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('booking_detail.html/', views.BookingList.as_view(), name='Booking Detail'),
+    path('booking_form.html/', views.BookingList.as_view(), name='Booking Detail'),
 ]
