22b2125e1b63cefd87408ca0eab54bf80c26a6bc
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-18 11:06:02 +0100

    Update template and views to fix TemplateSyntaxError

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,53 +1,52 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Restaurant
-from .forms import RestaurantForm
 
 class RestaurantList(generic.ListView):
     model = Restaurant
+    queryset: Restaurant.objects.all().order_by('name')
     template_name = 'restaurants/restaurant_list.html'
+    pagination = 8
 
-    context = {
-        'restaurants': Restaurant.objects.all()
-    }
+    # context = {
+    #     'restaurants': Restaurant.objects.all()
+    # }
 
     
-    """
-    Get data from forms.py and render in restaurant_form
-    """
+    # """
+    # Get data from forms.py and render in restaurant_form
+    # """
 
-    def get(self, request): 
-        return render(
-            request, "restaurants/restaurant_list.html",
-            {
-                "restaurant_form": RestaurantForm()
-            }
+    # def get(self, request): 
+    #     return render(
+    #         request, "restaurants/restaurant_list.html",
+    #         {
+    #             "restaurant_form": RestaurantForm()
+    #         }
            
-        )
+    #     )
 
-    """
-    Post data to database 
-    """
+    # """
+    # Post data to database 
+    # """
 
-    def post(self, request):
+    # def post(self, request):
+    #     if request.method == 'POST':
+    #         restaurant_form = RestaurantForm(request.POST)
+    #         if restaurant_form.is_valid():
+    #             restaurant = restaurant_form.save(commit=False)
+    #             restaurant.restaurant_form = restaurant_form
+    #             restaurant.save()
+    #             return redirect('/')
+    #         else:
+    #             restaurant_form = RestaurantForm()
 
-        # booking_form = get_object_or_404()
-        if request.method == 'POST':
-            restaurant_form = RestaurantForm(request.POST)
-            if restaurant_form.is_valid():
-                restaurant = restaurant_form.save(commit=False)
-                restaurant.restaurant_form = restaurant_form
-                restaurant.save()
-                return redirect('/')
-            else:
-                restaurant_form = RestaurantForm()
-
-        return render(
-            request, 
-            "restaurants/restaurant_list.html", 
-            {
-                "restaurant_form": RestaurantForm()
-            }
-        )
+    #     return render(
+    #         request, 
+    #         "restaurants/restaurant_list.html", 
+    #         {
+    #             "restaurant_form": RestaurantForm()
+    #         }
+    #     )
 
 
