096f602fc3653e8003406134dd6be13c35289ed8
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-19 16:59:52 +0100

    Add menu model, review and like functionalities

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Menu
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register Restaurant, review models.
-admin.site.register(Restaurant)
+# admin.site.register(Restaurant)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(SummernoteModelAdmin):
+    """ Add Restaurant model to admin page"""
+    list_filter = ('name', 'address',)
+    list_display = ('name', 'address', 'rating',)
+    search_fields = ('name', 'Restaurant',)
+
+@admin.register(Menu)
+class ReviewAdmin(SummernoteModelAdmin):
+    """ Add Menu model to admin page"""
+    list_filter = ('status', 'created_on',)
+    list_display = ('name', 'price', 'status', 'created_on',)
+    search_fields = ('date', 'time', 'number_of_guests', 'cuisine',)
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
     """ Add Review model to admin page"""
--- restaurants/models.py
+++ restaurants/models.py
@@ -21,7 +21,54 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=100)
     reserve = models.CharField(max_length=100, null=True)
     rating = models.IntegerField(blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='restaurant_likes', blank=True
+    )
     featured_image = CloudinaryField()
+
+    def __str__(self):
+        return str(self.name)
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Menu(models.Model):
+    '''Menu model
+    ---
+    Attributes:
+        name: Name of the cuisine 
+        menu: Details of the food to be served
+        price: Price of the cuisine
+        cuisine: Type of cuisine such as Chinese, African, Irish, intercontinental
+        status: States whether the cuisine is ready or not
+        cuisine_image: Photo image of the cuisine
+        created_on: Date and time created
+        updated_on: Date and time updated
+
+    '''
+
+    STATUS = ((0, "Not Ready"), (1, "Ready")) 
+
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name='restaurant_menu'
+    )
+    name = models.CharField(max_length=100, null=True)
+    description = models.TextField(max_length=500, null=True)
+    price = models.FloatField()
+    cuisine = models.PositiveSmallIntegerField(choices=(
+        (1, 'African'),
+        (2, 'Chinese'),
+        (3, 'Asian'),
+        (1, 'Irish'),
+        (1, 'Continental'),
+    ), null=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    cuisine_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-name"]
 
     def __str__(self):
         return str(self.name)
