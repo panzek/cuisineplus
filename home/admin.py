30046cb613d577ffe42dec2659969429f2c3028a
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-10-31 09:48:37 +0000

    Create home model and update

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Home
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(Home)
+class HomeAdmin(SummernoteModelAdmin):
+    """ Add Home model to admin page"""
+    summernote_fields = ('content', 'address',)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Create home models.
+class Home(models.Model):
+    '''Home model
+    ---
+    Attributes:
+        content: Introductory text content to the website
+        address: Pyshical address of Cuisine Plus
+        hero_image: Hero image of the homepage
+
+    '''
+    content = models.TextField(max_length=300, null=True)
+    address = models.CharField(max_length=100)
+    hero_image = CloudinaryField()
+    logo_image = CloudinaryField(null=True)
+
+    def __str__(self):
+        return str(self.content)
+
