3feafc373088e242a2722e3347e0079ddc014ec4
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-02-25 13:20:38 +0000

    Set debug to false in production

--- cuisine_plus/settings.py
+++ cuisine_plus/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cuisine-plus.herokuapp.com', 'localhost', '127.0.0.1',]
 CORS_ALLOWED_ORIGINS = [
